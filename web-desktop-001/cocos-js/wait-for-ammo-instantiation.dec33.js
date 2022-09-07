60d5417bbdf7af56463186bb375927463d3c8e5d
haleevgit <108408301+haleevgit@users.noreply.github.com>
2022-09-07 14:30:55 +0300

    Add files via upload

---
+++ web-desktop-001/cocos-js/wait-for-ammo-instantiation.dec33.js
@@ -0,0 +1,2 @@
+System.register(["./instantiated-b525ee6a.js"],(function(e){"use strict";return{setters:[function(t){e("default",t.he)}],execute:function(){}}}));
+//# sourceMappingURL=wait-for-ammo-instantiation.js.map
